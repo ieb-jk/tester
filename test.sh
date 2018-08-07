7829664afedcef238ce1b6c21e1c5628e5080129
EC2 Default User <ec2-user@ip-10-90-0-152.eu-west-1.compute.internal>
2018-08-07 21:50:01 +0000

    Testing

--- test.sh
+++ test.sh
@@ -1,7 +1,2 @@
-export newrepo=$(pwd)
-cd ~/workspace/jktest3
-echo “we are in $(pwd)”
-rm -rf build
-cp -rf $newrepo/build .
-#wget https://raw.githubusercontent.com/adambro/php-jenkins-example/master/build/phpdox.xml -O build/phpdox/build.xml
+cd ..
 ./vendor/bin/phpdox -f build/phpdox/build.xml
